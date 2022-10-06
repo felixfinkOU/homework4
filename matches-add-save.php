66de7df93f04b8a30030314e25f3ee0ed292a4b9
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-06 13:54:08 -0500

    test

--- matches-add-save.php
+++ matches-add-save.php
@@ -18,10 +18,10 @@ $iHomeTeamGoals = $_POST['iHomeTeamGoals'];
 $iAwayTeamGoals = $_POST['iAwayTeamGoals'];
 $iMatchday = $_POST['iMatchday'];
 
-$sql = "insert into Matches (HomeTeam, AwayTeam, HomeTeamGoals, AwayTeamGoals, Matchday) value ($iClub, $iStandings, $iHomeTeamGoals, $iAwayTeamGoals, $iMatchday)";
+$sql = "insert into Matches (MatchID, HomeTeam, AwayTeam, HomeTeamGoals, AwayTeamGoals, Matchday) value (?,?,?,?,?,?)";
 //echo $sql;
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssiii", $iClub, $iStandings, $iHomeTeamGoals, $iAwayTeamGoals, $iMatchday);
+    $stmt->bind_param("issiii", $iClub, $iStandings, $iHomeTeamGoals, $iAwayTeamGoals, $iMatchday);
     $stmt->execute();
 ?>
     
