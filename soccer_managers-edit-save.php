221c3e843150473a771b5e2a4505b4f928292e43
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-11-08 14:34:28 -0600

    try

--- soccer_managers-edit-save.php
+++ soccer_managers-edit-save.php
@@ -14,29 +14,38 @@ if ($conn->connect_error) {
 }
 
 $iCoachID = $_POST['iCoachID'];
+$iFirstName = $_POST['iFirstName'];
+$iLastName = $_POST['iLastName'];
+$iClub = $_POST['iClub'];
 
-if (isset($_POST['iFirstName'])) {
-    $iFirstName = $_POST['iFirstName'];
-    $sql = "UPDATE SoccerManagers set FirstName=? where CoachID=?";
+$sql = "UPDATE SoccerManagers set FirstName=?, LastName=?, Club=? where CoachID=?";
+//echo $sql;
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("si", $iFirstName, $iCoachID);
+    $stmt->bind_param("sssi", $iFirstName, $iLastName, $iClub, $iCoachID);
     $stmt->execute();
-}
-elseif (isset($_POST['iLastName'])){
-    $iLastName = $_POST['iLastName'];
-    $sql = "UPDATE SoccerManagers set LastName=? where CoachID=?"; 
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("si", $iLastName, $iCoachID);
-    $stmt->execute();   
-}
-elseif (isset($_POST['iClub'])){
-    $iClub = $_POST['iClub'];
-    $sql = "UPDATE SoccerManagers set Club=? where CoachID=?"; 
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("si", $iClub, $iCoachID);
-    $stmt->execute();   
-}
 ?>
+// if (isset($_POST['iFirstName'])) {
+//     $iFirstName = $_POST['iFirstName'];
+//     $sql = "UPDATE SoccerManagers set FirstName=? where CoachID=?";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->bind_param("si", $iFirstName, $iCoachID);
+//     $stmt->execute();
+// }
+// elseif (isset($_POST['iLastName'])){
+//     $iLastName = $_POST['iLastName'];
+//     $sql = "UPDATE SoccerManagers set LastName=? where CoachID=?"; 
+//     $stmt = $conn->prepare($sql);
+//     $stmt->bind_param("si", $iLastName, $iCoachID);
+//     $stmt->execute();   
+// }
+// elseif (isset($_POST['iClub'])){
+//     $iClub = $_POST['iClub'];
+//     $sql = "UPDATE SoccerManagers set Club=? where CoachID=?"; 
+//     $stmt = $conn->prepare($sql);
+//     $stmt->bind_param("si", $iClub, $iCoachID);
+//     $stmt->execute();   
+// }
+// ?>
     
     <h1>Edit Manager</h1>
 <div class="alert alert-success" role="alert">
