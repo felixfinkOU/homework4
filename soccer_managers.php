643070e8db87987af50b11dc814bfdeef0789aef
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-11-16 13:50:51 -0600

    test managers pop up

--- soccer_managers.php
+++ soccer_managers.php
@@ -105,7 +105,59 @@ $conn->close();
     </table>
 
 <a class="btn btn-primary" type="button" href="index.php">Go Back</a>
-<a href="soccer_managers-add.php" class="btn btn-primary">Add New</a>
+<!-- <a href="soccer_managers-add.php" class="btn btn-primary">Add New</a> -->
+
+<div>
+  <button type="button" style="color:white;background-color:green;" class="btn" data-bs-toggle="modal" data-bs-target="#addManager">
+    Add new
+  </button>
+  <div class="modal fade" id="addManager" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addManagerLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="addManagerLabel">Add Manager</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form method="post" action="soccer_managers-add-save.php">
+            <div class="mb-3">
+              <label for="FirstName" class="form-label">First Name</label>
+              <input type="text" class="form-control" id="FirstName" aria-describedby="firstNameHelp" name="iFirstName">
+              <div id="firstNameHelp" class="form-text">Enter the first name of the manager.</div>
+            </div>
+            <div class="mb-3">
+              <label for="LastName" class="form-label">Last Name</label>
+              <input type="text" class="form-control" id="LastName" aria-describedby="lastNameHelp" name="iLastName">
+              <div id="lastNameHelp" class="form-text">Enter the last name of the manager.</div>
+            </div>
+            <div class="mb-3">
+              <label for="Club" class="form-label">Club</label>
+              <select class="form-select" aria-label="Select Club" id="Club" name="iClub">
+              <?php
+                  $homeTeamSql = "select * from Teams order by Club";
+                  $homeTeamResult = $conn->query($homeTeamSql);
+                  while($homeTeamRow = $homeTeamResult->fetch_assoc()) {
+                    if ($homeTeamRow['Club'] == $row['Club']) {
+                      $selText = " selected";
+                    } else {
+                      $selText = "";
+                    }
+              ?>
+                <option value="<?=$homeTeamRow['Club']?>"<?=$selText?>><?=$homeTeamRow['Club']?></option>
+              <?php
+                  }
+              ?>
+              </select>
+              <div id="lastNameHelp" class="form-text">Enter the club of the manager.</div>
+            </div>
+            <input type="hidden" name="saveType" value="Add">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <?php include 'footer.php';?>
